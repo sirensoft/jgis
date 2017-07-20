dbf2719893945ee9b74b29fcf0f31f6b6c29c6bb
tehnn <tehnnn@gmail.com>
2017-07-20 13:19:46 +0700

    Thu 07/20/2017 13:19:46.68

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
+var fs = require('fs');
 var condb = require('../condb');
 var connection = mysql.createConnection(condb);
 
 /////////   logic //////////////////////
 
-
+router.get('/favicon.ico', function(req, res) {
+    res.status(204);
+});
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express' });
+	res.render('index');
+});
+
+router.get('/maptest', function(req, res, next) {
+	res.render('maptest');
 });
 
 router.get('/test',function(req,res){
@@ -31,6 +38,40 @@ router.post('/add',function(req,res){
 	});
 
 	//res.json(req.body)
+})
+
+router.get('/write',function(req,res){
+	var sql = "SELECT t.hcode,t.hno,v.villname,RIGHT(t.villcode,2) villno";
+	sql+= " ,t.ygis lat,t.xgis lng FROM house t LEFT JOIN village v on v.villcode = t.villcode";
+	var houseCollection=  {
+		"type": "FeatureCollection",
+		"name": "house",
+		"features":[]
+	};
+	connection.query(sql, function (err, result, fields) {
+		if (err) throw err;
+		result.forEach(function(row){
+			houseCollection.features.push({
+				"type": "Feature",
+				"properties": { 
+					'hcode':row.hcode,
+					'hno':row.hno,
+					'villno':row.villno,
+					'villname':row.villname, 
+					'title': row.hno +' หมู่ '+row.villno +' บ.'+row.villname, 
+					'marker-symbol':'warehouse',  
+    				'marker-color':'#0000FF',
+
+					
+				},
+				"geometry": { "type": "Point", "coordinates": [JSON.parse(row.lng),JSON.parse(row.lat)] } 
+			})
+		});//end Loop
+
+	fs.writeFile('house.json', houseCollection, function (err) {
+  	if (err) return console.log(err);
+  		console.log('Hello World > helloworld.txt');
+	});
 })
 
 router.get('/house',function(req,res){
